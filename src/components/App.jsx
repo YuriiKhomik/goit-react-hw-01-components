f5628c8064df0cc13e1adfd2be856d854e1838a1
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-02-16 15:11:23 +0200

    ...

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,14 +15,14 @@ export const App = () => {
       alignItems="center"
       flexDirection="column"
     >
-      {/* <Profile
+      <Profile
         username={user.username}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics title="Upload stats" stats={data} /> */}
+      <Statistics title="Upload stats" stats={data} />
 
       <FriendList friends={friends} />
     </Box>
